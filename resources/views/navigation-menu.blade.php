124132969131367ef9a5e0c2aa8b820a9108de1a
Ajmal-hassan <ajmalhassan651@gmail.com>
2025-06-30 18:57:37 +0500

    header issue fixed

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -237,6 +237,22 @@
                 </div>
             </div>
             @endauth
+            <div class="flex items-center ml-2 space-x-2 sm:hidden">
+                        <a
+                        href="{{ route('login') }}"
+                        class="w-20 px-2 py-1 text-sm bg-white dark:bg-gray-700 border border-gray-300 dark:border-gray-600 rounded-md dark:text-white font-medium shadow hover:bg-gray-100 dark:hover:bg-gray-600 transition text-blue-800 hover:bg-blue-200"
+                        >
+                            Login
+                        </a>
+                        <a
+                            href="{{ route('register') }}"
+                            class="w-20 px-2 py-1 text-sm bg-white dark:bg-gray-700 border border-gray-300 dark:border-gray-600 rounded-md dark:text-white font-medium shadow hover:bg-gray-100 dark:hover:bg-gray-600 transition text-blue-800 hover:bg-blue-200"
+                        >
+                            Register
+                        </a>
+                    
+
+                    </div>
 
                 <button
                         x-data="{ dark: document.documentElement.classList.contains('dark') }"
@@ -249,14 +265,14 @@
                         <span x-show="dark">🌙</span>
                 </button>
             <!-- Hamburger -->
-            <div class="-me-2 flex items-center sm:hidden">
+           <!--  <div class="-me-2 flex items-center sm:hidden">
                 <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
                     <svg class="size-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                         <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                     </svg>
                 </button>
-            </div>
+            </div> -->
         </div>
     </div>
 
